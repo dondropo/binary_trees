1bacc7e7836cdc7ebb7b1e57faaea7d3a01e0a7d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-07-21 16:26:22 +0000

    [Fixed] Task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -24,7 +24,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = parent->right;
 	parent->right = new_node;
-	new_node->left = NULL;
 
 	if (new_node->right != NULL)
 		new_node->right->parent = new_node;
