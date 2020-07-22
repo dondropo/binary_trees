eced7e614ab9861e58e1f3c41ae4cd7a71d7000f
dondropo <alejandroruscamoreno@gmail.com>
2020-07-22 21:19:05 +0000

    [Fix]: changed IF statement condition

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,8 +7,7 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL || node->parent)
+	if (node == NULL || node->parent)
 		return (0);
-	else
-		return (1);
+	return (1);
 }
